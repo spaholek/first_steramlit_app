b472b2a0302608e81587e7cb776fcb22839bbf99
spaholek <127778139+spaholek@users.noreply.github.com>
2023-03-16 11:44:26 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,4 +23,4 @@ streamlit.dataframe(fruits_to_show)
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text('Booger')
+streamlit.text(fruityvice_response)
